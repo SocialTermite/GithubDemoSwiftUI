dc8a4dd2a7a78f9ffd5491ca7172d0ce303fa776
Konstantin Bondar <bondar.k.dev@gmail.com>
2024-02-04 20:36:46 +0100

    Initial Commit

---
+++ GithubDemoSwiftUI/GithubDemoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubDemoSwiftUIApp.swift
+//  GithubDemoSwiftUI
+//
+//  Created by Konstantin Bondar on 04.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct GithubDemoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
